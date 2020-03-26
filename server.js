d13914078c579a9f50152152706ecaa547043970
Mauricio Marin <marinmauricio66@gmail.com>
2020-03-25 23:29:53 -0700

    Add routes structure

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+const users = require("./routes/users");
+const auth = require("./routes/auth");
+const contacts = require("./routes/contacts");
+
+app.use("/api/users", users);
+app.use("/api/auth", auth);
+app.use("/api/contacts", contacts);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}...`));
