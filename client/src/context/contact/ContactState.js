54d777ae43576a43c416ea67cf183e7286f59fd3
Mauricio Marin <marinmauricio66@gmail.com>
2020-04-06 14:18:22 -0700

    Set current clicked contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -37,7 +37,8 @@ const ContactState = props => {
         phone: "555-555-8888",
         type: "professional"
       }
-    ]
+    ],
+    current: null
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -61,8 +62,14 @@ const ContactState = props => {
   };
 
   // Set current contact
+  const setCurrent = contact => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
 
   // Clear current contact
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
 
   // Update contact
 
@@ -74,8 +81,11 @@ const ContactState = props => {
     <ContactContaxt.Provider
       value={{
         contacts: state.contacts,
+        current: state.current,
         addContact,
-        deleteContact
+        deleteContact,
+        setCurrent,
+        clearCurrent
       }}
     >
       {props.children}
