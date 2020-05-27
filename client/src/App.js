10f82892d00e532f2944078f37fe53787d4baaed
Mauricio Marin <marin@Mauricios-MacBook-Pro.local>
2020-05-26 17:26:11 -0700

    Add login and loadUser actions

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
-import React, { Fragment } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
 
-import Navbar from "./components/layout/Navbar";
-import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import Alerts from "./components/layout/Alerts";
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
 
-import AuthState from "./context/auth/AuthState";
-import ContactState from "./context/contact/ContactState";
-import AlertState from "./context/alert/AlertState";
+import AuthState from './context/auth/AuthState';
+import ContactState from './context/contact/ContactState';
+import AlertState from './context/alert/AlertState';
+
+import setAuthToken from './utils/setAuthToken';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
   return (
@@ -21,13 +27,13 @@ const App = () => {
           <Router>
             <Fragment>
               <Navbar />
-              <div className="container">
+              <div className='container'>
                 <Alerts />
                 <Switch>
-                  <Route exact path="/" component={Home} />
-                  <Route exact path="/about" component={About} />
-                  <Route exact path="/register" component={Register} />
-                  <Route exact path="/login" component={Login} />
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
                 </Switch>
               </div>
             </Fragment>
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,28 +1,33 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext } from 'react';
 
-import AlertContext from "../../context/alert/alertContext";
-import AuthContext from "../../context/auth/authContext";
+import AlertContext from '../../context/alert/alertContext';
+import AuthContext from '../../context/auth/authContext';
 
-const Register = () => {
+const Register = (props) => {
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
   const alertContext = useContext(AlertContext);
   const authContext = useContext(AuthContext);
 
   const { setAlert } = alertContext;
-  const { error, register, clearErrors } = authContext;
-
-  const [user, setUser] = useState({
-    name: "",
-    email: "",
-    password: "",
-    password2: "",
-  });
+  const { register, clearErrors, isAuthenticated, error } = authContext;
 
   useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push('/');
+    }
     if (error) {
-      setAlert(error, "danger");
+      setAlert(error, 'danger');
       clearErrors();
     }
-  }, [error]);
+
+    // eslint-disable-next-line
+  }, [isAuthenticated, error, props.history]);
 
   const { name, email, password, password2 } = user;
 
@@ -34,67 +39,67 @@ const Register = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    if (name === "" || email === "" || password === "") {
-      setAlert("Please enter all fields", "danger");
+    if (name === '' || email === '' || password === '') {
+      setAlert('Please enter all fields', 'danger');
     } else if (password !== password2) {
-      setAlert("Passwords do not match");
+      setAlert('Passwords do not match');
     } else {
       register({ name, email, password });
     }
   };
 
   return (
-    <div className="form-container">
+    <div className='form-container'>
       <h1>
-        Account <span className="text-primary">Register</span>
+        Account <span className='text-primary'>Register</span>
       </h1>
       <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
+        <div className='form-group'>
+          <label htmlFor='name'>Name</label>
           <input
-            type="text"
-            name="name"
+            type='text'
+            name='name'
             value={name}
             onChange={onChange}
             required
           />
         </div>
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
+        <div className='form-group'>
+          <label htmlFor='email'>Email</label>
           <input
-            type="email"
-            name="email"
+            type='email'
+            name='email'
             value={email}
             onChange={onChange}
             required
           />
         </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
+        <div className='form-group'>
+          <label htmlFor='password'>Password</label>
           <input
-            type="password"
-            name="password"
+            type='password'
+            name='password'
             value={password}
             onChange={onChange}
             required
-            minLength="6"
+            minLength='6'
           />
         </div>
-        <div className="form-group">
-          <label htmlFor="password2"> Confirm Password</label>
+        <div className='form-group'>
+          <label htmlFor='password2'> Confirm Password</label>
           <input
-            type="password"
-            name="password2"
+            type='password'
+            name='password2'
             value={password2}
             onChange={onChange}
             required
-            minLength="6"
+            minLength='6'
           />
         </div>
         <input
-          type="submit"
-          value="Register"
-          className="btn btn-primary btn-block"
+          type='submit'
+          value='Register'
+          className='btn btn-primary btn-block'
         />
       </form>
     </div>
