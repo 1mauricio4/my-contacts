53809c96f3c83b1f641b156f2d689c09056012d1
Mauricio Marin <marin@Mauricios-MacBook-Pro.local>
2020-06-01 16:18:25 -0700

    set static folder and scripts for deployment

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const path = require('path');
 
-const connectDB = require("./config/db");
+const connectDB = require('./config/db');
 
 // connect database
 connectDB();
@@ -9,13 +10,23 @@ connectDB();
 // Init middleware
 app.use(express.json({ extended: false }));
 
-const users = require("./routes/users");
-const auth = require("./routes/auth");
-const contacts = require("./routes/contacts");
+const users = require('./routes/users');
+const auth = require('./routes/auth');
+const contacts = require('./routes/contacts');
 
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-app.use("/api/contacts", contacts);
+app.use('/api/users', users);
+app.use('/api/auth', auth);
+app.use('/api/contacts', contacts);
+
+// Server static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
